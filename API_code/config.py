6bd992726ba4325472140c482ec571dbdf337137
Kodufan <64276501+Kodufan@users.noreply.github.com>
2022-03-30 20:08:17 -0400

    No I didn't push a password to the repo

--- API_code/config.py
+++ API_code/config.py
@@ -1,3 +1,4 @@
+# DB and API run on the same machine. DB only accepts localhost connections, so this password in the repo is not a vulnerability.
 DATABASE_URL = "mysql+mysqldb://CSE201_API:q58VEZ0HXyl1c7q1@127.0.0.1:3306/cse201_app?charset=utf8"
 ACCESS_TOKEN_DELTA_MINUTES = 15
 TOKEN_LENGTH = 64
