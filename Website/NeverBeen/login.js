950e8ee2e1876f54172f176904157ade11f0ee12
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2022-04-06 17:20:38 -0400

    Changed sign up function call and inserted success/failure messages to the user after attempting to create an account

--- Website/NeverBeen/login.js
+++ Website/NeverBeen/login.js
@@ -1,4 +1,6 @@
 
+const NeverBeenAPI = "http://134.53.116.212:8000";
+
 // Function used in tab switching
 function tabSelect(event, tabName){
     // Declare all variables
@@ -21,91 +23,59 @@ function tabSelect(event, tabName){
     event.currentTarget.className += " active";
 }
 
-// API call when user fills out the sign up form
-const NeverBeenAPI = "http://134.53.116.212:8000";
-window.addEventListener( "load", function () {
-    function sendData() {
-        const XHR = new XMLHttpRequest();
-        // Collect information from the form object
-        const jsonFormData = buildJsonFormData(form);
-        // Define what happens on successful data submission
-        XHR.addEventListener( "load", function(event) {
-            alert( event.target.responseText );
-        } );
-        // Define what happens in case of error
-        XHR.addEventListener( "error", function( event ) {
-            alert( 'Oops! Something went wrong.' );
-        } );
-        // Set up our request
-        XHR.open("POST", NeverBeenAPI + "/createUser", true);
-        XHR.setRequestHeader("Content-type", "application/json");
-        // The data sent is what the user provided in the form
-        XHR.send(JSON.stringify({
-            "username": jsonFormData["username"],
-            "email": jsonFormData["email"],
-            "rawPassword": jsonFormData["rawPassword"]
-        }));
-    }
-
-    // Access the form element...
-    const form = document.getElementById( "Signup" );
-    // ...and take over its submit event.
-    form.addEventListener( "submit", function ( event ) {
-        event.preventDefault();
-
-        sendData();
-    } );
-} );
-
-function buildJsonFormData(form) {
-    const jsonFormData = { };
-    for(const pair of new FormData(form)) {
-        jsonFormData[pair[0]] = pair[1];
-    }
-    return jsonFormData;
+// Check if "access_token" is in local storage
+// If it is, check if the token is valid
+// If it is valid, then the user is logged in and the user is redirected to the home page
+// If it is invalid, remove the access token from local storage and redirect the user to the login page
+// This is incomplete so far
+var access_token = localStorage.getItem("access_token");
+if(access_token != null){
+    window.location.href = "index.html";
 }
 
-// // API call when user submits login form
-// var jwt = localStorage.getItem("jwt");
-// console.log(jwt);
-// if (jwt != null) {
-//   window.location.href = './index.html'
-// }
+// Function that detects when the "Signup" form is submitted
+// When the form is submitted, send the form information to the "/createUser" endpoint in the NeverBeen API
+// If the user is successfully created, then the user is redirected to index.html
+console.log(document.getElementById("Signup"));
+document.getElementById("Signup").onsubmit = function(event){
+    event.preventDefault();
+    var username = document.getElementsByName("username")[0].value; // 1st instance of the username name
+    var email = document.getElementsByName("email")[1].value; // 2nd instance of the email name
+    var password = document.getElementsByName("rawPassword")[1].value; // 2nd instance of the rawPassword name
+    var XHR = new XMLHttpRequest();
+    XHR.open("post", NeverBeenAPI+"/createUser");
+    XHR.setRequestHeader("Content-type", "application/json");
+    XHR.send(JSON.stringify({
+        "username": username,
+        "email": email,
+        "rawPassword": password
+    }));
+    XHR.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 201){ // 201 is the status code for "Created"
+            // The user was successfully created
+            // Show the user a success message
+            Swal.fire({
+                title: "Success!",
+                text: "Your account has been created! Check your email to verify your account.",
+                icon: "success",
+                confirmButtonText: "Continue"
+            }).then(function(){
+                // Redirect the user to the home page
+                window.location.href = "index.html";
+            });
+        } else {
+            // The user was not created
+            // Send error message to the user
+            Swal.fire({
+                title: "Error!",
+                text: "There was an error creating your account."+this.responseText,
+                icon: "error",
+                confirmButtonText: "Continue"
+            }).then(function(){
+                // Refresh the login page
+                window.location.href = "login.html";
+            });
+        }
+    }
+};
 
-// function login() {
-//   const username = document.getElementById("email").value;
-//   const password = document.getElementById("rawPassword").value;
-
-//   const xhttp = new XMLHttpRequest();
-//   xhttp.open("POST", NeverBeenAPI+"/login");
-// //   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//   xhttp.send(JSON.stringify({
-//     "username": username,
-//     "password": password
-//   }));
-// //   xhttp.onreadystatechange = function () {
-// //     if (this.readyState == 4) {
-// //       const objects = JSON.parse(this.responseText);
-// //       console.log(objects);
-// //       if (objects['access_token'] != null) {
-// //         localStorage.setItem("jwt", objects['access_token']);
-// //         Swal.fire({
-// //           text: "You have been successfully signed in!",
-// //           icon: 'success',
-// //           confirmButtonText: 'OK'
-// //         }).then((result) => {
-// //           if (result.isConfirmed) {
-// //             window.location.href = './index.html';
-// //           }
-// //         });
-// //       } else {
-// //         Swal.fire({
-// //           text: "error",
-// //           icon: 'error',
-// //           confirmButtonText: 'OK'
-// //         });
-// //       }
-// //     }
-// //   };
-//   return false;
-// }
