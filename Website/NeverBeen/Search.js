c1d6d576f6d586239a46f842350849739f3a9344
bean1291 <99768497+bean1291@users.noreply.github.com>
2022-04-12 16:45:32 -0400

    Search
    api call

--- Website/NeverBeen/Search.js
+++ Website/NeverBeen/Search.js
@@ -1,31 +1,30 @@
-function searchShow(){
-const NeverBeenAPI = "http://134.53.116.212:8000/places/guest?order=Popularity&latitude=0&longitude=0&skip=0&limit=100";
-fetch(NeverBeenAPI)
-.then(response=> response.json())
-.then((jsonData)=>{
-  const results = jsonData.map(element => element.show.name);
-  renderResults(results);
-});
-}
+ function searchShow(){
+ const NeverBeenAPI = "http://134.53.116.212:8000/places/guest?order=Popularity&latitude=0&longitude=0&skip=0&limit=5";
+ fetch(NeverBeenAPI)
+ .then(response=> response.json())
+ .then((jsonData)=>{
+   console.log(jsonData);
+   const results = jsonData.map(element => element.friendlyName);
+   renderResults(results);
+ });
+ }
+//
+// let searchTimeoutToken = 0;
+//
+ function renderResults(results){
+   const list = document.getElementById("resultsList");
+   list.innerHTML = "";
+   results.forEach(result => {
+     const element = document.createElement("li");
+     element.innerText = result;
+     list.appendChild(element);
+   });
 
-let searchTimeoutToken = 0;
-
-function renderResults(results){
-  const list = document.getElementById("resultsList");
-  list.innerHTML = "";
-  results.forEach(result => {
-    const element = document.createElement("li");
-    element.innerText = result;
-    list.appendChild(element);
-
-  });
-
-}
-
-window.onload = () =>{
-  const searchFieldElement = document.getElementById("searchField");
-  searchFieldElement.onkeyup = (event) => {
-    searchShow(searchFieldElement.value);
-
-  };
-}
+ }
+//
+ window.onload = () =>{
+   const searchFieldElement = document.getElementById("searchField");
+   searchFieldElement.onkeyup = (event) => {
+     searchShow(searchFieldElement.value);
+   };
+ }
