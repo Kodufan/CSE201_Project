9f5bbd9be648db9d09913bdbe3e30a6c73af48af
bean1291 <99768497+bean1291@users.noreply.github.com>
2022-04-12 12:33:06 -0400

    Search Bar JS progress
    Incomplete, but working on it still

---
+++ Website/NeverBeen/Search.js
@@ -0,0 +1,31 @@
+function searchShow(){
+const NeverBeenAPI = "http://134.53.116.212:8000/places/guest?order=Popularity&latitude=0&longitude=0&skip=0&limit=100";
+fetch(NeverBeenAPI)
+.then(response=> response.json())
+.then((jsonData)=>{
+  const results = jsonData.map(element => element.show.name);
+  renderResults(results);
+});
+}
+
+let searchTimeoutToken = 0;
+
+function renderResults(results){
+  const list = document.getElementById("resultsList");
+  list.innerHTML = "";
+  results.forEach(result => {
+    const element = document.createElement("li");
+    element.innerText = result;
+    list.appendChild(element);
+
+  });
+
+}
+
+window.onload = () =>{
+  const searchFieldElement = document.getElementById("searchField");
+  searchFieldElement.onkeyup = (event) => {
+    searchShow(searchFieldElement.value);
+
+  };
+}
